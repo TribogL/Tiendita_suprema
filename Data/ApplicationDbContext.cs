1927bb0e19dd1e187eb07e7bd910d11a22c3fce4
Tribog <julix4212@gmail.com>
2024-10-12 23:49:58 -0500

    feat: The services for category were created.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 
 using Microsoft.EntityFrameworkCore;
+using Tiendita_suprema.Models;
 
 namespace Tiendita_suprema.Data;
 
 public class ApplicationDbContext : DbContext
 {
+    public DbSet<Category> Categories { get; set; }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
 
